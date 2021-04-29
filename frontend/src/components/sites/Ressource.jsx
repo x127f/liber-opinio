16f6637522e2e2319646b953025abc2e990c1af6
xnacly <matteogropp@protonmail.com>
2021-04-29 15:02:21 +0200

    added regex test

--- frontend/src/components/sites/Ressource.jsx
+++ frontend/src/components/sites/Ressource.jsx
@@ -1,5 +1,11 @@
 function Ressource({ match }) {
-	const ressource = match.params.ressource_url;
+	const ressource = decodeURIComponent(match.params.ressource_url);
+	if (
+		!/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g.test(
+			ressource
+		)
+	) {
+	}
 	return (
 		<div>
 			<p>{ressource}</p>
