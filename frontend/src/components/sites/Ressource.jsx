12a654281f07a66a11f78ab6cc6eb68e637950af
xnacly <matteogropp@protonmail.com>
2021-04-29 15:05:54 +0200

    revert last comit

--- frontend/src/components/sites/Ressource.jsx
+++ frontend/src/components/sites/Ressource.jsx
@@ -1,11 +1,5 @@
 function Ressource({ match }) {
 	const ressource = decodeURIComponent(match.params.ressource_url);
-	if (
-		!/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g.test(
-			ressource
-		)
-	) {
-	}
 	return (
 		<div>
 			<p>{ressource}</p>
