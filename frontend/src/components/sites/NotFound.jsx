d65639e6d08735af65ef171d34480aac18b64a7b
xnacly <matteogropp@protonmail.com>
2021-04-29 22:08:24 +0200

    improved 404-page

--- frontend/src/components/sites/NotFound.jsx
+++ frontend/src/components/sites/NotFound.jsx
@@ -1,5 +1,11 @@
-const NotFound = () => {
-	return <div>404</div>;
+const NotFound = ({
+	text = "The requested ressource wasn't found on our servers. If you believe this was an error, contact the admins via github/E-Mail.",
+}) => {
+	return (
+		<div className="not_found">
+			404 <span className="smaller_text">{text}</span>
+		</div>
+	);
 };
 
 export default NotFound;
