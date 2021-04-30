f4e1d20acd81b01cc1323c2a5c602ec34a10bb87
xnacly <matteogropp@protonmail.com>
2021-04-30 10:59:57 +0200

    color route

---
+++ frontend/src/components/sites/Colorpalette.jsx
@@ -0,0 +1,20 @@
+function Colorpalette() {
+	return (
+		<div className="showcase_container1">
+			<div className="showcase_container">
+				<button className="button">Button Default</button>
+				<button className="button success">Button Success</button>
+				<button className="button warning">Button Warning</button>
+				<button className="button danger">Button Danger</button>
+			</div>
+			<div className="showcase_container">
+				<p className="text">Text Default</p>
+				<p className="text quotetext">Text Quotetext</p>
+				<p className="text second">Text Second</p>
+				<p className="text third">Text Third</p>
+			</div>
+		</div>
+	);
+}
+
+export default Colorpalette;
